c59ad452072204b33157619e977e3a193a1db94d
Milan Pavkovic <pauk960@gmail.com>
2018-07-06 15:48:23 +0200

    Change primary location marker to home icon

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,13 @@
+import { Icon } from 'leaflet';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Map as LeafletMap, Marker, Polygon, Popup, TileLayer } from 'react-leaflet';
 import { connect } from 'react-redux';
+
+const homeIcon = new Icon({
+  iconUrl: 'https://cdn3.iconfinder.com/data/icons/map-markers-1/512/residence-512.png',
+  iconSize: [42, 42],
+});
 
 const Map = (props) => {
   const { activeFilter, layers, locations } = props;
@@ -15,51 +21,60 @@ const Map = (props) => {
         />
         {
           locations.length > 0
-            ? locations.map(l => (
-              <Marker key={l.id} position={[l.lat, l.lng]}>
-                <Popup className="map-popup-location" minWidth={200}>
-                  {l.image ? <img className="map-popup-location-image" src={l.image} /> : null}
-                  <div className="map-popup-location-wrapper">
-                    <div className="map-popup-location-type">
-                      {l.type}
+            ? locations.map((l) => {
+              const markerProps = {
+                key: l.id,
+                position: [l.lat, l.lng],
+              };
+              if (l.isPrimary) {
+                markerProps.icon = homeIcon;
+              }
+              return (
+                <Marker {...markerProps}>
+                  <Popup className="map-popup-location" minWidth={200}>
+                    {l.image ? <img className="map-popup-location-image" src={l.image} /> : null}
+                    <div className="map-popup-location-wrapper">
+                      <div className="map-popup-location-type">
+                        {l.type}
+                      </div>
+                      <div className="map-popup-location-address">
+                        <span className="map-popup-location-address-street">
+                          {l.address.street}
+                        </span>
+                        <br />
+                        <span className="map-popup-location-address-city">
+                          {`${l.address.city}, ${l.address.zipCode}`}
+                        </span>
+                      </div>
+                      <div className="map-popup-location-beds">
+                        <span className="map-popup-location-label">
+                          BEDS
+                        </span>
+                        {l.beds}
+                      </div>
+                      <div className="map-popup-location-baths">
+                        <span className="map-popup-location-label">
+                          BATHS
+                        </span>
+                        {l.baths}
+                      </div>
+                      <div className="map-popup-location-size">
+                        <span className="map-popup-location-label">
+                          SQ. FT
+                        </span>
+                        {new Intl.NumberFormat('en-US').format(l.size)}
+                      </div>
+                      <div className="map-popup-location-price">
+                        <span className="map-popup-location-label">
+                          LIST PRICE
+                        </span>
+                        {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0 }).format(l.price)}
+                      </div>
                     </div>
-                    <div className="map-popup-location-address">
-                      <span className="map-popup-location-address-street">
-                        {l.address.street}
-                      </span>
-                      <br />
-                      <span className="map-popup-location-address-city">
-                        {`${l.address.city}, ${l.address.zipCode}`}
-                      </span>
-                    </div>
-                    <div className="map-popup-location-beds">
-                      <span className="map-popup-location-label">
-                        BEDS
-                      </span>
-                      {l.beds}
-                    </div>
-                    <div className="map-popup-location-baths">
-                      <span className="map-popup-location-label">
-                        BATHS
-                      </span>
-                      {l.baths}
-                    </div>
-                    <div className="map-popup-location-size">
-                      <span className="map-popup-location-label">
-                        SQ. FT
-                      </span>
-                      {new Intl.NumberFormat('en-US').format(l.size)}
-                    </div>
-                    <div className="map-popup-location-price">
-                      <span className="map-popup-location-label">
-                        LIST PRICE
-                      </span>
-                      {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0 }).format(l.price)}
-                    </div>
-                  </div>
-                </Popup>
-              </Marker>
-            ))
+                  </Popup>
+                </Marker>
+              );
+            })
             : null
         }
         {
